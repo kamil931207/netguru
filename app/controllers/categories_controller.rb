82c224c42b841cafa710a650d6b5d7aeeab206c6
Kamil Czelen <kamil.czelen@hybris.com>
2017-05-12 17:56:34 +0200

    refactor of categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :fetch_category, only: [:show, :edit, :update, :destroy]
+
 
   def index
   	# binding.pry
@@ -8,12 +10,12 @@ class CategoriesController < ApplicationController
   def new
   	@category = Category.new
   end
- 
+
   def create
   	# narzedzie do debugowania
   	# binding.pry
-  	# potem w konsoli mozesz np. params 
-  	#  
+  	# potem w konsoli mozesz np. params
+  	#
   	# raise params.to_yaml
   	# Category.create(category_params)
   	@category = Category.new(category_params)
@@ -30,28 +32,26 @@ class CategoriesController < ApplicationController
 
   # metody ktore nic nie robia w jednej linijce
   # def show; end
-  def show
-  	@category = Category.find(params[:id])
-  end
+  def show; end
 
-  def edit
-  	@category = Category.find(params[:id])
-  end
+  def edit; end
 
   def update
-  	@category = Category.find(params[:id])
   	@category.update_attributes(category_params)
   	redirect_to @category
   end
 
   def destroy
-  	@category = Category.find(params[:id])
   	@category.destroy!
   	flash[:notice] = "Category #{@category.name} deleted"
   	redirect_to categories_path
   end
 
   private
+
+  def fetch_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
   	# tylko zapisze name
