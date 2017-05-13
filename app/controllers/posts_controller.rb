b89f5861cfdd3db4939d842be49ceab45dbbd8f4
Kamil Czelen <kamil.czelen@hybris.com>
2017-05-13 12:44:05 +0200

    post functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :fetch_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.reverse
+  end
+
+  def user_posts
+    @posts = current_user.posts
+  end
+
+  def new
+      @post = Post.new
+  end
+
+  def create
+    return unless post_params[:user_id]== current_user.id.to_s
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created'
+      # redirect_back(fallback_location: root_path)
+      # przekierowanie gdzies
+      redirect_to @post
+    else
+      flash[:alert] = 'Post not created'
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post.update_attributes(post_params)
+    redirect_to @post
+  end
+
+  def edit; end
+
+  def destroy
+    @post.destroy!
+    flash[:notice] = "Post #{@post.title} deleted"
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :expiration_date, :user_id)
+  end
+
+  def fetch_post
+    @post = Post.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homepage#index'
 
+  resources :posts
+
   resources :categories
 
   get '/about', to: 'homepage#about'
 
   get '/cookies', to: 'homepage#cookies'
+
+  get '/my_posts', to: 'posts#user_posts'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
